49c0c7e421ac590c09a0bcc0b3bf188b9f1b8a7b
Lucas <lucas.evans.carpio@gmail.com>
2015-12-18 14:57:28 -0400

    Color fixed, login added

--- app/src/main/java/com/example/lucascarpio/route/MainActivity.java
+++ app/src/main/java/com/example/lucascarpio/route/MainActivity.java
@@ -59,38 +59,19 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.places) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.events) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.routes) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.log_out) {
 
         }
 
