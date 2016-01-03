8858ee191deb70225e50a0f3d1264c023ef6f294
Fabio Emmanuel Ferreras Vasquez <fabio_ferreras@hotmail.com>
2016-01-03 17:54:05 -0400

    Check your current position

--- app/src/main/java/com/example/lucascarpio/route/MainActivity.java
+++ app/src/main/java/com/example/lucascarpio/route/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.lucascarpio.route;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -13,6 +17,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -23,6 +28,10 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.parse.Parse;
 import com.parse.ParseUser;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
@@ -70,12 +79,74 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         GPSTracker gpsTracker = new GPSTracker(this);
         gpsTracker.getLocation();
+
+        double latitude = gpsTracker.getLatitude();
+        double longitude = gpsTracker.getLongitude();
+
         Marker marker = googleMap.addMarker(new MarkerOptions()
-                .position(new LatLng(gpsTracker.getLatitude(),gpsTracker.getLongitude()))
+                .position(new LatLng(latitude, longitude))
                 .title("You're here"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(gpsTracker.getLatitude(), gpsTracker.getLongitude()), 15));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), 15));
+
+        checkLocation(latitude,longitude);
+    }
+
+
+    //Check if the current position is inside the area of the Colonial Zone
+    public void checkLocation(double latitude, double longitude) {
+
+        Geocoder geocoder= new Geocoder(this, Locale.ENGLISH);
+        try {
+
+            //Place your latitude and longitude
+            List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
+
+            if(addresses != null && addresses.size() > 0) {
+                Address address = addresses.get(0);
+                String subLocality = address.getSubLocality();
+                if(subLocality.equals("Ciudad Colonial"))
+                {
+                    Toast.makeText(this, "Estas en la Ciudad Colonial", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    Toast.makeText(this, "No estas en la Ciudad Colonial", Toast.LENGTH_LONG).show();
+                }
+            }
+            else
+            {
+                Toast.makeText(this, "Can't get your current location", Toast.LENGTH_LONG).show();
+            }
+
+
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(),"Could not get address..!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private static class GeocoderHandler extends Handler {
+
+        private String mLocation = null;
+
+        @Override
+        public void handleMessage(Message message) {
+            String locationAddress;
+            switch (message.what) {
+                case 1:
+                    Bundle bundle = message.getData();
+                    locationAddress = bundle.getString("address");
+                    break;
+                default:
+                    locationAddress = null;
+            }
+            mLocation = locationAddress;
+        }
     }
 
     @Override
